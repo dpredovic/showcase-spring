eaa813df0d8ee3b401a3f91a4d381d7a3e4b183b
Dejan Predovic <dp@1und1.de>
2012-03-20 12:12:55 +0100

    ear packaging cleanup ejb remoting test cleanup

--- showcase-service/showcase-service-ear/src/test/java/showcase/service/ear/RemoteInvocation.java
+++ showcase-service/showcase-service-ear/src/test/java/showcase/service/ear/RemoteInvocation.java
@@ -4,6 +4,7 @@ import java.util.Hashtable;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import org.junit.Before;
 import org.junit.Test;
 import showcase.service.api.CustomerService;
 import showcase.service.api.dto.CustomerDto;
@@ -12,16 +13,22 @@ import static org.fest.assertions.Assertions.assertThat;
 
 public class RemoteInvocation {
 
+    private CustomerService customerService;
+
+    @Before
+    public void setUp() throws Exception {
+        Hashtable jndiProperties = new Hashtable();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        Context context = new InitialContext(jndiProperties);
+
+        customerService = (CustomerService) context.lookup(CustomerService.JNDI_NAME);
+    }
+
     @Test
     @SuppressWarnings("unchecked")
     public void testServiceCall() throws Exception {
-        final Hashtable jndiProperties = new Hashtable();
-        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-        final Context context = new InitialContext(jndiProperties);
-
-        CustomerService customerService = (CustomerService) context.lookup(CustomerService.JNDI_NAME);
-
-        CustomerDto byId = customerService.getById(1L);
-        assertThat(byId.getId()).isEqualTo(1);
+        long id = 421L;
+        CustomerDto customer = customerService.getById(id);
+        assertThat(customer.getId()).isEqualTo(id);
     }
 }
